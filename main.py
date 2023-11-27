31b75708756fc82fe7723031181def4f02192be8
Сat Cheshire <catmagic000@gmail.com>
2023-11-27 09:49:29 +0300

    Update main.py
    The save logic and general logic have been corrected

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ hellostring="""
 2 сохранить в файл
 3 добавить заметку
 4 редактировать заметку
-5 найти по записи
-6 удалить заметку 
-7 выйти из программы
+5 найти заметки по дате
+6 читать по номеру заметки
+7 удалить заметку 
+8 выйти из программы
 введите номер команды:
 """
 def save(db):
+	print("файл сохранен")
     pass
 def need_save(flag_need_save,db,string):
     while flag_need_save:
         print(string)
         numbercomand=input()
         if is_number_int(numbercomand):
-            if(int(numbercomand)==1 or int(numbercomand)==2):
+            if int(numbercomand) == 1 or int( numbercomand ) == 2 :
                 flag_need_save=False
-                save(db)
+                if int(numbercomand) == 1:
+                    save(db)
             else:
                 print("команды с таким номером нет")
         else:
@@ -42,20 +45,27 @@ if __name__ == '__main__':
             numbercomand=input()
             if is_number_int(numbercomand):
                 numbercomand=int(numbercomand)
-                if  0<numbercomand and numbercomand < 8:
+                if  0<numbercomand and numbercomand < 9:
                     flag_correct_numercomand=True
                     if(numbercomand ==1):#команда загрузить
                         need_save(flag_need_save, db, "Вы хотите загрузить файл,но у вас есть не сохранные изменения.\nсохранить?\n1 да\n2 нет")
+                        flag_need_save=False
+                        pass
                     elif( numbercomand == 2):
                         save(db)
-                        pass
+                        flag_need_save=False
                     elif(numbercomand==3):
+                    	file_need_save=True
                         pass
                     elif(numbercomand==4):
+                    	file_need_save=True
                         pass
                     elif(numbercomand==5):
                         pass
                     elif(numbercomand==6):
+                        pass
+                    elif(numbercomand==7):
+                        file_need_save=True
                         pass
                     else:#команда выхода
                         flag_exit=True
@@ -65,5 +75,6 @@ if __name__ == '__main__':
                 print("Был введен не номер команды")
             if flag_correct_numercomand==False:
                 print("введите номер командыы заново")
-        if flag_exit==True:
+        if flag_exit == True:
+        	need_save(flag_need_save,db,"Вы хотите выйти, но у вас есть не сохранные изменения.\nсохранить?\n1 да\n2 нет")
             break
